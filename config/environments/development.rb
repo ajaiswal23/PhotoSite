d0391f367e65456b6d83d24c1197def6039d3bc4
Aditya <addyjaiswal2309@gmail.com>
2020-03-02 15:52:33 -0800

    adding config.hosts to develpement file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+
+  config.hosts << /[a-z0-9]+\.c9users\.io/
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
